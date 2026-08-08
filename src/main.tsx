7e0ef1dbe9763358a02ee22ce961fbf21cce754f
BIBLE_MAP <skyhyangsu63@gmail.com>
2026-08-08 17:31:44 +0900

    Mount worship workflow hub and section anchors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import SectionAnchorInstaller from './SectionAnchorInstaller'
+import WorshipWorkflowHub from './WorshipWorkflowHub'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <SectionAnchorInstaller />
+    <WorshipWorkflowHub />
     <App />
   </React.StrictMode>,
 )
