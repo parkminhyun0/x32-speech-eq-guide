8f5e06205e0f163c86b4c3e8a2085a57b5fcf57a
BIBLE_MAP <skyhyangsu63@gmail.com>
2026-08-08 17:32:24 +0900

    Load workflow hub layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import SectionAnchorInstaller from './SectionAnchorInstaller'
 import WorshipWorkflowHub from './WorshipWorkflowHub'
 import './styles.css'
+import './workflow-layout.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
