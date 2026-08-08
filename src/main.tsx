3b59162e478dd5fdda2e73592dd7462118e30315
BIBLE_MAP <skyhyangsu63@gmail.com>
2026-08-08 22:21:19 +0900

    Mount X32 Connect quick navigation shortcut

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import LocalSecureContextNotice from './LocalSecureContextNotice'
 import SectionAnchorInstaller from './SectionAnchorInstaller'
 import WorshipWorkflowHub from './WorshipWorkflowHub'
 import X32Connect from './X32Connect'
+import X32ConnectMenuShortcut from './X32ConnectMenuShortcut'
 import './styles.css'
 import './workflow-layout.css'
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SectionAnchorInstaller />
     <WorshipWorkflowHub />
+    <X32ConnectMenuShortcut />
     <LiveMonitorShortcut />
     <LocalSecureContextNotice />
     <div className="x32-connect-root"><X32Connect /></div>
